1eede2cb4f23810395780f9056611deb21b2e8e7
nnammo7676 <nnammo7676@gmail.com>
2022-10-13 23:27:33 +0700

    done

--- src/app.js
+++ src/app.js
@@ -2,15 +2,17 @@ const express = require('express');
 // require('express) ben trong modulues de luu vao bien express 
 const app = express();
 //khi khoi tao function sau khi khoi tao no se tra ve 1 doi tuong de xay dung 1 website 
-const port = process.env.PORT || 3000
+const port = 3000;
 //Bien port de minh co the khoi chay theo cong tuyen 3000
 const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 const path = require('path');
 app.use(morgan('combined'));
 // Template engine
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
+app.engine('hbs', engine({
+  extname:'.hbs'
+}));
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 
@@ -19,7 +21,6 @@ app.get('/', (req, res) => {
 })
 
 
-app.listen(port, () => {
-  console.log(`App listening at ${port}`)
-  //http:localhost:3000
+app.listen(process.env.PORT,function () {
+  
 })
